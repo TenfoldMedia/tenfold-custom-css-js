1a3191013e7cca7f6ac0015f06b16739fd4e755a
Tim Rye <tim@tenfold.media>
2016-02-23 10:49:01 +0000

    Bug fix: Enqueue site-wide files using correct actions
    Fixes site-wide JS and improves performance of site-wide CSS.

--- tenfold-custom-css-js.php
+++ tenfold-custom-css-js.php
@@ -5,7 +5,7 @@ Plugin URI:        http://tenfold.media
 Description:       Renders CSS and JS added to pages/posts using the 'wp_head' and 'wp_footer' hooks. Note that ACF must be active and the field group must be set up - this plugin does not do that. Also enqueues sitewide CSS and JS files if they exist in the current theme directory (as tf-custom.css and tf-custom.js).
 Author:            Tim Rye
 Author URI:        http://tenfold.media/tim
-Version:           1.0.3
+Version:           1.0.4
 GitHub Plugin URI: TenfoldMedia/tenfold-custom-css-js
 GitHub Branch:     master
 ******************************************************************/
@@ -13,19 +13,28 @@ GitHub Branch:     master
 $priority = 1e6;
 
 
-function tf_render_sitewide_css() {
+function tf_enqueue_sitewide_css() {
 	if (file_exists(get_stylesheet_directory() . '/tf-custom.css')) {
 		wp_enqueue_style('tf-sitewide-css', get_stylesheet_directory_uri() . '/tf-custom.css');
 	}
 }
-add_action('wp_head', 'tf_render_sitewide_css', $priority - 1);
+add_action('wp_print_styles', 'tf_enqueue_sitewide_css', $priority);
+// theory behind using wp_print_styles action:
+//   this is as late as we can reliably get it without it falling to the footer
+//   this even gets it below Jetpack's 'super late priority' css
+//   the only things it wont override are inline styles in the body
 
-function tf_render_sitewide_js() {
+function tf_enqueue_sitewide_js() {
 	if (file_exists(get_stylesheet_directory() . '/tf-custom.js')) {
 		wp_enqueue_script('tf-sitewide-js', get_stylesheet_directory_uri() . '/tf-custom.js', array('jquery'), '', true);
 	}
 }
-add_action('wp_footer', 'tf_render_sitewide_js', $priority - 1);
+add_action('get_footer', 'tf_enqueue_sitewide_js');
+// theory behind using get_footer action:
+//   get_footer is the action that runs during get_footer()
+//   get_footer() is the function that calls the footer.php template file
+//   by the time we get to footer.php, all other plugins and stuff should have enqueued their scripts, even if they enqueue them during the body render
+//   hence this action should enqueue our site-wide script after every other script, but before the page-specific JS below
 
 
 function tf_render_custom_css() {
